523f7c67939468e35908f41ff4586aaebb512034
keanarichards <62271282+keanarichards@users.noreply.github.com>
2020-05-21 11:19:37 -0400

    set up
    adding raw data, original 699 paper, and initial loading & cleaning script

---
+++ source/00_load_raw_data_and_clean.R
@@ -0,0 +1,26 @@
+# load packages -----------------------------------------------------------
+
+
+## Package names"
+packages <- c("readr")
+
+## Install packages not yet installed
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+
+## Packages loading
+invisible(lapply(packages, library, character.only = TRUE))
+
+
+
+# load data ---------------------------------------------------------------
+
+raw <- read_csv("data/raw.csv")
+
+
+
+
+
+## 
